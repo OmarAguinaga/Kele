af1504dad2f290ed3dec527c51c4384427984932
Omar <omar.aguinaga94@gmail.com>
2017-08-03 23:52:57 +0000

    kele with test working

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,10 +2,11 @@ require "httparty"
 
 class Kele
   include HTTParty
-  base_uri = "https://www.bloc.io/api/v1"
-
-  def initialize( email, password )
-    response = self.class.post( "/sessions", body: { "email": email, "password": password } )
+  base_uri "https://www.bloc.io/api/v1"
+  
+  def initialize(email, password)
+    response = self.class.post( "/sessions", body: {"email": email, "password": password } )
+    raise "Invalid email or password" if response.code == 404
     @auth_token = response["auth_token"]
   end
 end
--- spec/kele_spec.rb
+++ spec/kele_spec.rb
@@ -5,10 +5,11 @@ RSpec.describe Kele, type: :request do
   include HTTParty
   context '.kele' do
     describe '#initialize' do
-      it "authenticates user" do
-        client = Kele.new("jane@gmail.com", "abc123")
-        expect(client.instance_variable.get(:@auth_token)).to be_a String
+      it 'authenticates user' do
+        response = Kele.new("omar.aguinaga94@gmail.com", "pw")
+        expect(response.instance_variable_get(:@auth_token)).to be_a String
       end
+
     end
   end
 end

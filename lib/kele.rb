d19eeeb0964948a625c842dfb9e8a9db86916a8b
Omar <omar.aguinaga94@gmail.com>
2017-08-07 14:49:04 +0000

    Add create messages method

--- lib/kele.rb
+++ lib/kele.rb
@@ -23,5 +23,27 @@ class Kele
     response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     @mentor_availability_data = JSON.parse(response.body)
   end
+  
+  def get_messages(page=nil)
+    if page == nil
+       response = self.class.get("/message_threads", headers: { "authorization" => @auth_token })
+    else
+       response = self.class.get("/message_threads?page=#{page}", headers: { "authorization" => @auth_token })
+    end
+     @messages_data = JSON.parse(response.body)
+  end
+  
+  def create_message(user_email, recipient_id, token=nil, subject, message)
+    response = self.class.post("/messages",
+      body: {
+        "sender": user_email,
+        "recipient_id": recipient_id,
+        "token": token,
+        "subject": subject,
+        "stripped_text": message
+      },
+      headers: {"authorization" => @auth_token})
+    puts response
+  end
 
 end

56d03ae263b149488d5c84fdf3f580ff93eddc59
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-08-09 20:22:07 -0500

    Add method to submit a checkpoint

--- lib/kele.rb
+++ lib/kele.rb
@@ -45,16 +45,16 @@ class Kele
     puts response
   end
 
-  def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+  def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
     response = self.class.post("/checkpoint_submissions",
       body: {
         "checkpoint_id": checkpoint_id,
         "assignment_branch": assignment_branch,
         "assignment_commit_link": assignment_commit_link,
-        "comment": comment
+        "comment": comment,
+        "enrollment_id": enrollment_id
       },
       headers: { "authorization" => @auth_token })
     puts response
   end
-
 end
